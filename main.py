c3d52efb22550644da9794401bef7d5f69357fb1
Chathura J Ekanayake <chathuraje01@gmail.com>
2023-12-15 17:24:51 +0530

    Add logging to main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from libraries.reddit import get_top_reddit_post
 from libraries.screenshots import get_screenshots_of_reddit_posts
+from utils.log import setup_logger, get_logger
 
+setup_logger()
+logger = get_logger()
 
 def main():
-    subreddit = "AskReddit"
+    logger.info("Starting...")
     
+    subreddit = "AskReddit"
     reddit_data = get_top_reddit_post(subreddit)
     get_screenshots_of_reddit_posts(reddit_data)
+    
+    logger.info("Done.")
     
 if __name__ == "__main__":
     main()
