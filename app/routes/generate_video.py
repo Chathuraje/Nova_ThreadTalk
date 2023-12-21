0d2f6356fe6af44eb13a322f94a10f87892326fb
Chathura J Ekanayake <chathuraje01@gmail.com>
2023-12-22 00:16:07 +0530

    Add SubredditEnum to generate_short_video endpoint

--- app/routes/generate_video.py
+++ app/routes/generate_video.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.libraries import generate_video
 from utils.log import setup_logger, get_logger
+from enum import Enum
 
 setup_logger()
 logger = get_logger()
@@ -14,8 +15,11 @@ router = APIRouter(
 def read_root():
     return {"message": "Hello, this is your FastAPI application!"}
 
+class SubredditEnum(str, Enum):
+    AskReddit = "AskReddit"
+
 @router.get("/generate_short_video/{subreddit}")
-def generate_short_video(subreddit: str):
+def generate_short_video(subreddit: SubredditEnum):
     generate_video.generate_short_video(subreddit)
     
     return {"message": f"Short video successfully generated and uploaded...."}
