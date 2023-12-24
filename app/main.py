1e8b70761f3c8cf73f7a49cd40fb041525806ec3
Chathura J Ekanayake <chathuraje01@gmail.com>
2023-12-25 02:12:48 +0530

    Add CORS middleware to allow cross-origin requests

--- app/main.py
+++ app/main.py
@@ -4,9 +4,19 @@ from app.routes.google import router as google_routes
 from app.routes.tiktok import router as tiktok_routes
 from app.routes.setup import router as setup_routes
 from app.routes.video import router as video_routes
-# 
+from fastapi.middleware.cors import CORSMiddleware 
+
 app = FastAPI()
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+) 
 
 app.include_router(root_routers)   
 app.include_router(google_routes)   
